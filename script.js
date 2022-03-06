b5a135298f836a70eb4b51a6c10ee6c391d1001a
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-03-06 12:00:33 -0700

    added code for spec char and started creating spec char array

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var generateBtn = document.querySelector("#generate");
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowerCase =  ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numbers = [0,1,2,3,4,5,6,7,8,9];
-var specialChar = [];
+var specialChar = ['`','~','!','@','#','$','%','^','&','*','(',')','-','_','+','=','{','[',']','}'];
 
 // Password generator function
 function generatePassword() {
@@ -35,6 +35,11 @@ function generatePassword() {
   var useNumbers = window.confirm("Would you like to include numbers?");
   if (useNumbers) {
     allCharacters = allCharacters.concat(numbers);
+  };
+
+  var useCharacters = window.confirm("Would you like to include special characters?");
+  if (useCharacters) {
+    allCharacters = allCharacters.concat(specialChar);
   };
 
   console.log(allCharacters);
